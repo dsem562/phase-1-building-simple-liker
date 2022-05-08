3f88c58296b6029a2c4a4f77b7bd1ca73eb48409
David <dsem562@gmail.com>
2022-05-08 12:13:30 -0700

    Completed assignment

--- main.js
+++ main.js
@@ -4,7 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const init = () => {
 
+  const like = document.querySelectorAll("span.like-glyph");
+  const modal = document.getElementById("modal");
+  const errorMsg = document.querySelector("#modal h2");
+  
+  like.forEach(element => element.addEventListener("click", ifLiked));
+  
+  function ifLiked(e) { 
+    
+    if (e.target.innerHTML === EMPTY_HEART) {
+      
+      mimicServerCall()
+      .then(function () {  
+        e.target.innerHTML = FULL_HEART;
+        e.target.classList.add("activated-heart");
+      })
+      .catch(function (error) {      
+        modal.classList.remove("hidden");
+        errorMsg.innerHTML = error;
+        setTimeout(() => { modal.classList.add("hidden") }, 3000)
+      });
+    
+    } else {     
+      
+      e.target.innerHTML = EMPTY_HEART
+      e.target.classList.remove("activated-heart");
+    
+    }
+  };
+};
+
+document.addEventListener('DOMContentLoaded', init);
 
 
 //------------------------------------------------------------------------------
